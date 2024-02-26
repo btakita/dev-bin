4267723f2bb89f032722e8566541c4522b11b4c6
Brian Takita <brian.takita@gmail.com>
2024-02-26 14:31:07 -0500

    git_pull_rebase_submodules_parallel: seperate fetch & rebase

--- bin/git_pull_rebase_submodules_parallel.sh
+++ bin/git_pull_rebase_submodules_parallel.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
- git pull --recurse-submodules -j 16 --rebase && git submodule status | \
- awk '{print $2}' | \
- xargs -P16 -I{} git -C {} pull --rebase origin HEAD
+git fetch && \
+git rebase && \
+git submodule status | \
+awk '{print $2}' | \
+xargs -P16 -I{} git -C {} pull --rebase --no-recurse-submodules origin HEAD
