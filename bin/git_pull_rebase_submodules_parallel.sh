66aa114efd1b5caa96df635ddc2931b209d671c3
Brian Takita <brian.takita@gmail.com>
2024-02-26 14:26:16 -0500

    git_pull_rebase_submodules_parallel: format

--- bin/git_pull_rebase_submodules_parallel.sh
+++ bin/git_pull_rebase_submodules_parallel.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
- git pull --recurse-submodules -j 16 --rebase && git submodule status | awk '{print $2}' | xargs -P16 -I{} git -C {} pull --rebase origin HEAD
+ git pull --recurse-submodules -j 16 --rebase && git submodule status | \
+ awk '{print $2}' | \
+ xargs -P16 -I{} git -C {} pull --rebase origin HEAD
