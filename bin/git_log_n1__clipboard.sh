f85843ccd5fbd51c084a36fb33337540cfde3e8c
Brian Takita <brian.takita@gmail.com>
2024-03-22 18:52:01 -0400

    git_log_n1__clipboard: remove blank lines

--- bin/git_log_n1__clipboard.sh
+++ bin/git_log_n1__clipboard.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-(cd $1; git add .; git log -n1 | tail -n+5 | sed -En 's/ {4}/\t/gp') | xsel -b
+(cd $1; git add .; git log -n1 | tail -n+5 | sed -En 's/ {4}/\t/gp') | sed -r '/^\s*$/d' | xsel -b
